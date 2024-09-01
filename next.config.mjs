f88a9398eeab92029da107b88a4eb15076a6a5cb
markeloss <eriquepinon@gmail.com>
2024-09-01 19:20:44 -0300

    agora foi

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/</barbearia-simples-Devmedia->',
-    assetPrefix: '/</barbearia-simples-Devmedia->/',
+
 };
 
 export default nextConfig;
