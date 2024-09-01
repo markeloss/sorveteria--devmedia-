d0f40e3ae9993498a5724685603ba7393e8fbac1
markeloss <eriquepinon@gmail.com>
2024-08-31 21:40:10 -0300

    subindo o site

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/</barbearia-simples-Devmedia->',
+    assetPrefix: '/</barbearia-simples-Devmedia->/',
+};
+
+export default nextConfig;
